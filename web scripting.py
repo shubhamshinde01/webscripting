b65b52ec81e9f22d5ac161d89d6dd03765c97123
shubhamshinde01 <93381750+shubhamshinde01@users.noreply.github.com>
2021-10-30 12:15:20 +0530

    Update web scripting.py

--- web scripting.py
+++ web scripting.py
@@ -13,9 +13,7 @@ for page_num in range(1, page_num_MAX):
     content = req.content
 
     soup=BeautifulSoup(content, "html.parser")
-    all_hotels = soup.find_all("div", {"class": "hotelCardListing"})"
-
-   
+    all_hotels = soup.find_all("div", {"class": "hotelCardListing"})
     scraped_info_list = []
 
     for hotel in all_hotels:
@@ -26,23 +24,23 @@ for page_num in range(1, page_num_MAX):
 
      #try... except
 
-     try:
-         hotel_dict ["rating"]= hotel.find("span", {"class": "hotelRating_ratingSummary")).text
-         except AttributeError:
-             pass
+       try:
+           hotel_dict ["rating"]= hotel.find("span", {"class": "hotelRating_ratingSummary"}).text
+       except AttributeError:
+            pass
 
-     parent_amenities element = hotel.find("div", {"class": "amenityWrapper"}}
+       parent_amenities_element = hotel.find("div", {"class": "amenityWrapper"})
 
-     amenities_list = []
+       amenities_list = []
 
-     for amenity in parent_amenities element.find_all("div", {"class": "amenityWrapper__amenity"}):
-         amenities _list.append(amenity.find("span", {"class": "d-body-sm")).text.strip())
+       for amenity in parent_amenities_element.find_all("div", {"class": "amenityWrapper__amenity"}):
+           amenities_list.append(amenity.find("span", {"class": "d-body-sm"}).text.strip())
  
-     hotel_dict["amenities"] = ', .join(amenities_list[:-1])
+       hotel_dict["amenities"] = ', '.join(amenities_list[:-1])
  
-      scraped_info_list.append(hotel_dict)
+       scraped_info_list.append(hotel_dict)
 
-     #print (hotel_name, hotel address, hotel_price, hotel_rating, amenities_list)
+      #print (hotel_name, hotel address, hotel_price, hotel_rating, amenities_list)
 
 dataFrame = pandas.DataFrame(scraped_info_list)
 
